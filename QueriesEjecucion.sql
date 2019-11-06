80ccd4387e23e04216b8ba174af784e3b9ad96d6
rbarragan14 <ricardobarragano14@gmail.com>
2019-11-06 08:48:49 -0500

    Cambio hecho con el clienteÃ

--- QueriesEjecucion.sql
+++ QueriesEjecucion.sql
@@ -8,7 +8,7 @@ CALL SYSUIF.INSTALL_FILE('R_cox_train', 'R_cox_train.r', 'cz!C:\\Users\\rb255002
 SELECT  *
 FROM SCRIPT (
 ON (select  
- 1,Asegurado_id, Ind_Ramo_Canal_Vigente, Meses_Antiguedad_Ramo_Canal, Canal_Agrup_Id 
+ 1 c1,Cross_Validation,Asegurado_id ,  Ind_Ramo_Canal_Vigente, Meses_Antiguedad_Ramo_Canal, Canal_Agrup_Id 
 ,Ind_Exequial, Ind_Accidentes_Personales, Ind_Automoviles, Ind_Hogar, Ind_SuRenta 
  ,Ind_MasVida, Ind_Soat, Ind_Renta_Educativa, Ind_Otros_Personales, Ind_Salud 
   ,Ind_Otros_Empresariales,  Ind_Arrendamiento, Ind_Renta_Pensional, Ind_Desempleo, Ind_Vida 
@@ -16,18 +16,24 @@ ON (select
 ,Cont_Compania_Cliente ,   Regional_Contacto_id, Sexo_id, Edad, Numero_Hijos
   ,Ind_Dependientes_Menores18, Ind_Dependientes_Mayores18, Nivel_Ingreso_id, Nivel_Estudios_id, Estado_Civil_id  
  ,Ind_Independiente,  Cont_Quejas, Cont_Peticion,  Cont_Inquietud, Ind_Tipo_Negocio_Colectivo
-,Ind_Forma_Pago_Mensual, Ind_Forma_Pago_Anual, Ind_Tipo_Pago_Caja_Sura, Ind_Tipo_Pago_Financiacion_Sura ,1
-FROM Rtdsura.Training_VPC_Cancelacion_Autos2
+,Ind_Forma_Pago_Mensual, Ind_Forma_Pago_Anual, Ind_Tipo_Pago_Caja_Sura, Ind_Tipo_Pago_Financiacion_Sura 
+FROM Rtdsura.Training_Modelo_VPC_Autos2
 )
---partition by c1
+partition by c1
 SCRIPT_COMMAND('R --vanilla --slave -f ./Rtdsura/R_cox_train.r')
 --RETURNS ('time1 integer,delta integer,gender integer,race integer') )
 --RETURNS ('x1 decimal (18,1) , x2 decimal (18,10)')) ;
-RETURNS ('x1 decimal (18,14)'));
+RETURNS ('Aseg integer, T1 decimal (18,4) , T2 decimal (18,4)')) a
+INNER JOIN 
+Rtdsura.Training_Modelo_VPC_Autos2 b
+on b.Asegurado_id = a.Aseg
+;
+
+--RETURNS ('x1 decimal (18,14)'));
 
 SELECT DISTINCT *
     FROM SCRIPT (
-    SCRIPT_COMMAND('tail -n 3 /var/opt/teradata/tdtemp/uiflib/scriptlog')
+    SCRIPT_COMMAND('tail -n 19 /var/opt/teradata/tdtemp/uiflib/scriptlog')
         RETURNS ('x1 Varchar (400)') );
-        
+
 
--- R_cox_train.r
+++ R_cox_train.r
@@ -9,13 +9,13 @@ inputDF <- data.frame();
 # the colClasses argument of the read.table() function.
 #cv <- c("numeric","numeric","numeric","numeric")
 cv <- c("numeric", "numeric","numeric","numeric","numeric"
+		,"numeric","numeric","numeric","numeric", "numeric"
         ,"numeric","numeric","numeric","numeric", "numeric"
+		,"numeric","numeric","numeric","numeric", "numeric"
         ,"numeric","numeric","numeric","numeric", "numeric"
+		,"numeric","numeric","numeric","numeric", "numeric"
         ,"numeric","numeric","numeric","numeric", "numeric"
-        ,"numeric","numeric","numeric","numeric" ,  "numeric"
-        ,"numeric","numeric","numeric","numeric", "numeric"
-        ,"numeric","numeric","numeric","numeric", "numeric"
-        ,"numeric","numeric","numeric","numeric", "numeric"
+		,"numeric","numeric","numeric","numeric", "numeric"
         ,"numeric","numeric","numeric","numeric", "numeric")
 
 inputDF <- try(read.table(stdin, sep=DELIMITER, flush=TRUE, header=FALSE, quote="", na.strings="", colClasses=cv), silent=TRUE)
@@ -29,65 +29,56 @@ if (class(inputDF) == "try-error") {
 }
 #installed.packages("survival")
 library(survival)
+library(dplyr)
+
 # Create dataframe with model-dependent vars. Select them from the inputDF.
 
-names(inputDF)[1]="c1" 
-names(inputDF)[2]="Asegurado_id" 
-names(inputDF)[3]="Ind_Ramo_Canal_Vigente" 
-names(inputDF)[4]="Meses_Antiguedad_Ramo_Canal" 
-names(inputDF)[5]="Canal_Agrup_Id" 
 
-names(inputDF)[6]="Ind_Exequial" 
-names(inputDF)[7]="Ind_Accidentes_Personales" 
-names(inputDF)[8]="Ind_Automoviles" 
-names(inputDF)[9]="Ind_Hogar" 
-names(inputDF)[10]="Ind_SuRenta" 
-
-names(inputDF)[11]="Ind_MasVida" 
-names(inputDF)[12]="Ind_Soat" 
-names(inputDF)[13]="Ind_Renta_Educativa" 
-names(inputDF)[14]="Ind_Otros_Personales" 
-names(inputDF)[15]="Ind_Salud" 
-
-names(inputDF)[16]="Ind_Otros_Empresariales" 
-names(inputDF)[17]="Ind_Arrendamiento" 
-names(inputDF)[18]="Ind_Renta_Pensional" 
-names(inputDF)[19]="Ind_Desempleo" 
-names(inputDF)[20]="Ind_Vida" 
-
- names(inputDF)[21]="Produccion_Emitida_Grupo_Cliente" 
- names(inputDF)[22]="Porc_RT_Grupo_Cliente" 
- names(inputDF)[23]="Frecuencia_Anual_Siniestros_Grupo_Cliente" 
- names(inputDF)[24]="Monto_Pagado_Siniestros_Grupo_Cliente" 
-names(inputDF)[25]="Recencia_Meses_Ultimo_Siniestros_Grupo_Cliente" 
-
-names(inputDF)[26]="Cont_Compania_Cliente" 
-names(inputDF)[27]="Regional_Contacto_id" 
-names(inputDF)[28]="Sexo_id" 
-names(inputDF)[29]="Edad" 
-names(inputDF)[30]="Numero_Hijos" 
-
-names(inputDF)[32]="Ind_Dependientes_Mayores18" 
-names(inputDF)[33]="Nivel_Ingreso_id" 
-names(inputDF)[34]="Nivel_Estudios_id" 
-names(inputDF)[35]="Estado_Civil_id" 
-
-names(inputDF)[36]="Ind_Independiente" 
-names(inputDF)[37]="Cont_Quejas" 
-names(inputDF)[38]="Cont_Peticion" 
-names(inputDF)[39]="Cont_Inquietud" 
-names(inputDF)[40]="Ind_Tipo_Negocio_Colectivo" 
-
-names(inputDF)[41]="Ind_Forma_Pago_Mensual" 
-names(inputDF)[42]="Ind_Forma_Pago_Anual" 
-names(inputDF)[43]="Ind_Tipo_Pago_Caja_Sura" 
-names(inputDF)[44]="Ind_Tipo_Pago_Financiacion_Sura" 
-names(inputDF)[45]="Extra" 
-
-
-#
-#newdata0<-inputDF[c(1,500, 600, 700, 750,800),]
-library(survival)
+names(inputDF)[1]="c1"
+names(inputDF)[2]="Cross_Validation"
+names(inputDF)[3]="Asegurado_id"
+names(inputDF)[4]="Ind_Ramo_Canal_Vigente"
+names(inputDF)[5]="Meses_Antiguedad_Ramo_Canal"
+names(inputDF)[6]="Canal_Agrup_Id"
+names(inputDF)[7]="Ind_Exequial"
+names(inputDF)[8]="Ind_Accidentes_Personales"
+names(inputDF)[9]="Ind_Automoviles"
+names(inputDF)[10]="Ind_Hogar"
+names(inputDF)[11]="Ind_SuRenta"
+names(inputDF)[12]="Ind_MasVida"
+names(inputDF)[13]="Ind_Soat"
+names(inputDF)[14]="Ind_Renta_Educativa"
+names(inputDF)[15]="Ind_Otros_Personales"
+names(inputDF)[16]="Ind_Salud"
+names(inputDF)[17]="Ind_Otros_Empresariales"
+names(inputDF)[18]="Ind_Arrendamiento"
+names(inputDF)[19]="Ind_Renta_Pensional"
+names(inputDF)[20]="Ind_Desempleo"
+names(inputDF)[21]="Ind_Vida"
+names(inputDF)[22]="Produccion_Emitida_Grupo_Cliente"
+names(inputDF)[23]="Porc_RT_Grupo_Cliente"
+names(inputDF)[24]="Frecuencia_Anual_Siniestros_Grupo_Cliente"
+names(inputDF)[25]="Monto_Pagado_Siniestros_Grupo_Cliente"
+names(inputDF)[26]="Recencia_Meses_Ultimo_Siniestros_Grupo_Cliente"
+names(inputDF)[27]="Cont_Compania_Cliente"
+names(inputDF)[28]="Regional_Contacto_id"
+names(inputDF)[29]="Sexo_id"
+names(inputDF)[30]="Edad"
+names(inputDF)[31]="Numero_Hijos"
+names(inputDF)[32]="Ind_Dependientes_Menores18"
+names(inputDF)[33]="Ind_Dependientes_Mayores18"
+names(inputDF)[34]="Nivel_Ingreso_id"
+names(inputDF)[35]="Nivel_Estudios_id"
+names(inputDF)[36]="Estado_Civil_id"
+names(inputDF)[37]="Ind_Independiente"
+names(inputDF)[38]="Cont_Quejas"
+names(inputDF)[39]="Cont_Peticion"
+names(inputDF)[40]="Cont_Inquietud"
+names(inputDF)[41]="Ind_Tipo_Negocio_Colectivo"
+names(inputDF)[42]="Ind_Forma_Pago_Mensual"
+names(inputDF)[43]="Ind_Forma_Pago_Anual"
+names(inputDF)[44]="Ind_Tipo_Pago_Caja_Sura"
+names(inputDF)[45]="Ind_Tipo_Pago_Financiacion_Sura"
 
 
 inputDF$Canal_Agrup_Id <- factor(inputDF$Canal_Agrup_Id)
@@ -97,50 +88,27 @@ inputDF$Nivel_Ingreso_id <- factor(inputDF$Nivel_Ingreso_id)
 inputDF$Nivel_Estudios_id <- factor(inputDF$Nivel_Estudios_id)
 inputDF$Estado_Civil_id <- factor(inputDF$Estado_Civil_id)
 
+newdata0<-inputDF %>% filter(Cross_Validation==1)
+
+newdata1<-newdata0[,3:ncol(newdata0)]
 
 
+inputDF<-inputDF %>% filter(Cross_Validation==0)#para entrenar modelo
+inputDF1<-inputDF[,3:ncol(inputDF)]#excluye campos que no aplican
 
-#inputDF1=rbind(inputDF, unregistro)
-
-#modelocoxph <- coxph(Surv(time, delta) ~ race + gender , data = inputDF)
-modelocoxph <- coxph(Surv(Meses_Antiguedad_Ramo_Canal, Ind_Ramo_Canal_Vigente) ~ . , data = inputDF)
-
-#
-#modelo<-survfit(modelocoxph, conf.type = "log-log",type = "kaplan-meier",newdata =newdata0 )
+modelocoxph <- coxph(Surv(Meses_Antiguedad_Ramo_Canal, Ind_Ramo_Canal_Vigente) ~ . , data = inputDF1)
 
 
 save(modelocoxph, file="/tmp/modelocoxph.RData")
 
+modelo<-survfit(modelocoxph, conf.type = "log-log",type = "kaplan-meier",newdata =newdata1 )
 
-#modelo<-survfit(modelocoxph, conf.type = "log-log",type = "kaplan-meier",newdata =inputDF )
+#output=data.frame(modelo$surv[2,1:length(modelo$surv[2,])])
 
-
-
-
-#output = as.data.frame(modelo$surv[2,1:3])
-#output=data.frame(inputDF$id, modelo$surv[2,1:length(modelo$surv[2,])])
-output=data.frame(modelocoxph$coefficients)
-#output=data.frame(inputDF1$id)
-
-
-
-#output=data.frame(modelo$surv)
-
-#res<-summary(modelo,times=c(1,100))
-#res<-summary(modelo)
-
-#salida=data.frame(t(cbind(Tiempo=res$time,res$surv)))
-#out=salida$X2
-
-#out=inputDF
-
-#dataDF <- data.frame(out);
-
-# Build export data frame:
-# output <- data.frame(out);
-#output <- data.frame(salida);
-
+output=data.frame(Asegurado_id=newdata0[,3],Tiempo1=modelo$surv[4,1:length(modelo$surv[4,])],Tiempo2=modelo$surv[61,1:length(modelo$surv[61,])])
 
 # Export results to Teradata through standard output
-#write.table(output, file=stdout(), col.names=FALSE, row.names=FALSE,quote=FALSE, sep="\t", na="")
- write.table(output, file=stdout(), col.names=FALSE, row.names=FALSE,quote=FALSE, sep="\t", na="")
+
+write.table(output, file=stdout(), col.names=FALSE, row.names=FALSE,quote=FALSE, sep="\t", na="")
+
+# write.table(1, file=stdout(), col.names=FALSE, row.names=FALSE,quote=FALSE, sep="\t", na="")
